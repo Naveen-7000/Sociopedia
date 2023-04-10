fdf379b8f8d51a5433c4f0e0057b679bcf16efee
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-04-10 15:20:09 +0530

    key added to friends

--- src/db.js
+++ src/db.js
@@ -1,3 +1,3 @@
-// place https://locahost:3000/ or whatever your server for test don't forget to add / at the end
-export const BASE_URL = ""
+// place http://locahost:3000/ or whatever your server for test don't forget to add / at the end
+export const BASE_URL = "http://localhost:3000/"
 
--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -38,9 +38,9 @@ const FriendListWidget = ({ userId }) => {
         Friend List
       </Typography>
       <Box display="flex" flexDirection="column" gap="1.5rem">
-        {friends.map((friend) => (
+        {friends.map((friend,key) => (
           <Friend
-            key={friend._id}
+            key={key}
             friendId={friend._id}
             name={`${friend.firstName} ${friend.lastName}`}
             subtitle={friend.occupation}
